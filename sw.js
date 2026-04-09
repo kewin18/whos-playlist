ec45acb2e9a4d8d411a72fc9b2302fdfebfcf8ad
kewin18 <kewinfamulla54@gmail.com>
2026-04-09 19:58:43 +0200

    stabilize mobile runtime: prevent round races and reduce heavy rerenders

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "whos-playlist-cache-v8";
+﻿const CACHE_NAME = "whos-playlist-cache-v9";
 const URLS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
