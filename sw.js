d7b74d4fbddeff86e390e9ee720ddb519e800aa7
kewin18 <kewinfamulla54@gmail.com>
2026-04-09 20:41:17 +0200

    rework round flow: remove prep freeze, fix loading transition, cache v16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "whos-playlist-cache-v15";
+﻿const CACHE_NAME = "whos-playlist-cache-v16";
 const URLS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
