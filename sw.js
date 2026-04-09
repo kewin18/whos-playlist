9622536483357803d4f6b0e0bf8d64b1675795c6
kewin18 <kewinfamulla54@gmail.com>
2026-04-09 19:09:54 +0200

    fix preview loading with iTunes JSONP fallback + cache v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "whos-playlist-cache-v5";
+﻿const CACHE_NAME = "whos-playlist-cache-v6";
 const URLS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
