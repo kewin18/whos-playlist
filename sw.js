a4a058c6ce91f067d2ffb987d699ea0f7b2b356e
kewin18 <kewinfamulla54@gmail.com>
2026-04-09 19:15:28 +0200

    fix autoplay unlock and retry + cache v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "whos-playlist-cache-v6";
+﻿const CACHE_NAME = "whos-playlist-cache-v7";
 const URLS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
