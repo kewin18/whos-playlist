8d3472c871c8341c767bee5af8ec4b01bf9bfc5c
kewin18 <kewinfamulla54@gmail.com>
2026-04-09 20:24:42 +0200

    stabilize round start on mobile with hard payload timeouts and nextRound guard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "whos-playlist-cache-v12";
+﻿const CACHE_NAME = "whos-playlist-cache-v13";
 const URLS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
